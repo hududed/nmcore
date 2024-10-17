c551df7f959479f814a9c99691d87d18fe41c81d
hududed <hudwahab@gmail.com>
2024-10-16 21:52:32 -0600

    dark theme, first, last, email batchwrite

--- nmcore-landing/src/routes/+page.svelte
+++ nmcore-landing/src/routes/+page.svelte
@@ -2,35 +2,12 @@
     import NotifyForm from '$lib/components/NotifyForm.svelte';
 </script>
 
-<div class="container">
-    <div class="logo">
+<div class="card #172554 shadow-xl p-10 text-center">
+    <!-- <div class="logo">
         <img src="/favicon.png" alt="NanoGrowTech Logo">
-    </div>
-    <h1>nmCORE - Unlock More Yield with Revolutionary Nano Fertilizer</h1>
-    <p>Finally, a breakthrough solution that enhances photosynthesis, cuts fertilizer usage by 25%, and transforms your farm’s productivity. Stay tuned!</p>
+    </div> -->
+    <h1 class="text-green-400 text-4xl mb-5">nmCORE</h1>
+    <h1 class="text-4xl mb-5">Maximize Your Crop Growth with Nano Technology</h1>
+    <p class="text-lg mb-8">Finally, a breakthrough solution that enhances photosynthesis, cuts fertilizer usage by 25%, and transforms your farm’s productivity. Stay tuned!</p>
     <NotifyForm />
 </div>
-
-<style>
-    .container {
-        text-align: center;
-        background: white;
-        padding: 50px;
-        border-radius: 10px;
-        box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
-    }
-    h1 {
-        margin-bottom: 20px;
-        font-size: 2.5em;
-    }
-    p {
-        margin-bottom: 30px;
-        font-size: 1.2em;
-    }
-    .logo {
-        margin-bottom: 20px;
-    }
-    .logo img {
-        width: 150px;
-    }
-</style>
