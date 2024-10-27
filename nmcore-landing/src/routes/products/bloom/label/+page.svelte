be8269322b08c96d82d9182f6508c04df42e864e
hududed <hudwahab@gmail.com>
2024-10-27 07:18:06 -0600

    Add label, sellsheet, paper

---
+++ nmcore-landing/src/routes/products/bloom/label/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+</script>
+
+<style>
+    .pdf-container {
+      width: 100%;
+      height: 100vh; /* Full viewport height */
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
+  
+    .pdf-iframe {
+      width: 100%;
+      height: 100%;
+      border: none; /* Remove default border */
+    }
+  </style>
+
+
+<!-- Display PDF -->
+<div class="pdf-container">
+    <iframe class="pdf-iframe" src="/pdfs/BLOOM_label.pdf" title="BLOOM Sell Sheet"></iframe>
+  </div>
+
+
+<!-- Display PNG or JPG -->
+<!-- <img src="path/to/your/image.png" alt="Sell Sheet" width="100%" height="auto"> -->
